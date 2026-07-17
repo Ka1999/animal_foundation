fe7a76e2f6de27263434f6d07928796a0cf31f44
Ka1999 <karenvane1024@gmail.com>
2026-07-16 19:42:59 -0500

    Clase Persona

---
+++ src/modules/Persona.py
@@ -0,0 +1,10 @@
+class Persona: 
+    def __init__(self, 
+                 nombre: str,
+                 cedula: str
+                 ):
+        self.nombre: str = nombre
+        self.cedula: str = cedula
+
+
+        
